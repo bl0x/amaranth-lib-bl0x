99d0beba2b2f8d329e17d7129acdf16f000f0f0a
Bastian Löher <b.loeher@gsi.de>
2021-11-03 22:53:45 +0100

    Initial commit.

---
+++ histogram.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env nmigen
+
+from nmigen import *
+from nmigen.sim import *
+
+class Histogram(Elaboratable):
+
+    def __init__(self, bins=100, bits=8):
+        self.bins = bins
+        self.bits = bits
+        self.mem = Array([Signal(unsigned(self.bits)) for _ in range(bins)])
+        self.index = Signal(range(1, self.bins))
+        self.data = Signal(unsigned(self.bits))
+        self.increment = Signal()
+        self.write = Signal()
+        self.read = Signal()
+
+        self.ports = (
+                self.index,
+                self.data,
+                self.increment,
+                self.write,
+                self.read
+                )
+
+    def elaborate(self, platform):
+        m = Module()
+
+        with m.If(self.increment == 1):
+            m.d.sync += self.mem[self.index].eq(self.mem[self.index] + 1)
+        with m.Elif(self.read == 1):
+            m.d.sync += self.data.eq(self.mem[self.index])
+        with m.Elif(self.write == 1):
+            m.d.sync += self.mem[self.index].eq(self.data)
+
+        return m
+
+if __name__ == '__main__':
+    dut = Histogram(bins=8, bits=8)
+
+    sim = Simulator(dut)
+
+    def write(value, index):
+        # set a bin at index to a value
+        yield dut.index.eq(index)
+        yield dut.data.eq(value)
+        yield dut.write.eq(1)
+        yield Tick()
+        yield Settle()
+
+        # reset write
+        yield dut.write.eq(0)
+        yield dut.data.eq(0)
+        yield Tick()
+        yield Settle()
+
+    def read(index):
+        # read
+        yield dut.index.eq(index)
+        yield dut.read.eq(1)
+        yield Tick()
+        yield Settle()
+
+        # reset read
+        yield dut.read.eq(0)
+        yield Tick()
+        yield Settle()
+
+    def increment(index):
+        # increment
+        yield dut.index.eq(index)
+        yield dut.increment.eq(1)
+        yield Tick()
+        yield Settle()
+
+        # reset increment
+        yield dut.increment.eq(0)
+        yield Tick()
+        yield Settle()
+
+    def proc():
+        yield from write(24, 4)
+        yield from read(4)
+
+        print("dut.data = {}".format((yield dut.data)))
+        assert (yield dut.data) == 24, "Didn't get 24!"
+
+        yield from increment(4)
+        yield from read(4)
+
+        print("dut.data = {}".format((yield dut.data)))
+        assert (yield dut.data) == 25, "Didn't get 25!"
+
+        yield from increment(1)
+        yield from read(1)
+
+        print("dut.data = {}".format((yield dut.data)))
+        assert (yield dut.data) == 1, "Didn't get 1!"
+
+
+    sim.add_clock(1e-6)
+    sim.add_sync_process(proc)
+    with sim.write_vcd('histogram.vcd', 'histogram.gtkw', traces=dut.ports):
+        sim.run()
---
+++ pulser.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env nmigen
+
+from nmigen import *
+from nmigen.sim import *
+
+class Pulser(Elaboratable):
+
+    def __init__(self, period=10, length=1):
+        self.period = Signal(range(1, 10000000), reset=period)
+        self.length = Signal(range(1, 10000000), reset=length)
+        self.out = Signal()
+
+        self.ports = (
+                self.period,
+                self.length,
+                self.out
+                )
+
+    def elaborate(self, platform):
+        counter = Signal(range(0, 10000000), reset=0)
+        m = Module()
+
+        with m.If(counter == 0):
+            m.d.sync += counter.eq(self.period - 1)
+        with m.Else():
+            m.d.sync += counter.eq(counter - 1)
+
+        with m.If(counter < self.length):
+            m.d.sync += self.out.eq(1)
+        with m.Else():
+            m.d.sync += self.out.eq(0)
+
+        return m
+
+if __name__ == '__main__':
+    dut = Pulser(period=10, length=3)
+
+    sim = Simulator(dut)
+    def proc():
+        for i in range(200):
+            yield Tick()
+            yield Settle()
+
+    sim.add_clock(1e-6)
+    sim.add_sync_process(proc)
+    with sim.write_vcd('pulser.vcd', 'pulser.gtkw', traces=dut.ports):
+        sim.run()
