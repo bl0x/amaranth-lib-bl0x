3ea04778c7c79fea8564f0019ef2397d4b0187a9
Bastian Löher <b.loeher@gsi.de>
2021-11-25 01:11:19 +0100

    Work on clock divider.

--- xc7/mmcm.py
+++ xc7/mmcm.py
@@ -3,76 +3,115 @@ from nmigen.cli import main
 
 # supports a subset of the Xilinx Series 7 MMCME2_ADV features
 # inspired by / taken from:
+# https://github.com/kbob/nmigen-examples/blob/master/nmigen_lib/pll.py
+# and
 # https://github.com/enjoy-digital/litex/blob/master/litex/soc/cores/clock/xilinx_s7.py
+# check also this: https://41j.com/blog/2020/01/ice40hx8k-ad9225-adc-with-nmigen/
 
 class MMCME2(Elaboratable):
+    clkout0_divide_range = (1, (128 + 1/8), 1/8)
+    clkout_divide_range = (1, 128+1)
+    #clkfbout_mult_range = (2, 64+1)
+    divclk_divide_range = (1, 106+1)
+    clkin_range = (10e6, 800e6)
+    vco_range = (600e6, 1200e6)
 
     def __init__(self, freq_in, freq_out, domain_name='sync'):
         self.freq_in = freq_in
-        self.freq_out = freq_out
-        self.domain_name = domain_name
-        self.domain = ClockDomain(domain_name)
 
         self.clkin = Signal()
         self.reset = Signal()
-
         self.locked = Signal()
-        self.clkout0 = Signal()
-        self.clkout0_buf = ClockSignal(self.domain_name)
 
-        self.clkout0_divide_range = (1, (128 + 1/8), 1/8)
-        self.clkout_divide_range = (1, 128+1)
-        self.clkfbout_mult_range = (2, 64+1)
-        self.divclk_divide_range = (1, 106+1)
-        self.clkin_range = (10e6, 800e6)
-        self.vco_range = (600e6, 1200e6)
+        self.config = {}
 
-        self.config = self.make_config()
+        self.outputs = []
 
         self.ports = [
                 self.clkin,
-                self.domain.clk,
-                self.domain.rst
+                self.reset,
+                self.locked
                 ]
+
+        self.m = Module()
+
+    def create_clkout(self, domain_name, frequency, phase=0):
+        clkout = Signal()
+        clkout_buf = ClockSignal(domain_name)
+
+        self.outputs.append((clkout, frequency, phase))
+
+        self.m.submodules += Instance("BUFG",
+                i_I=clkout,
+                o_O=clkout_buf
+                )
+
+        self.m.domains += ClockDomain(domain_name)
+        self.m.d.comb += clkout.eq(clkout_buf)
 
     def make_config(self):
         config = {}
-        config["clkfbout_mult"] = 1
-        config["divclk_divide"] = 1
-        config["clkout0_freq"] = 1
-        config["clkout0_divide"] = 1
-        config["clkout0_phase"] = 1
+
+        # fixed for now
+        clkfbout_mult = 32
+        divclk_divide = 1
+        config["clkfbout_mult"] = clkfbout_mult
+        config["divclk_divide"] = divclk_divide
+
+        vco_freq = self.freq_in * clkfbout_mult / divclk_divide
+
+        valid = False
+        for n, (clk, freq, phase) in enumerate(self.outputs):
+            for d in range(*self.clkout_divide_range):
+                f = vco_freq / d
+                if f == freq:
+                    config[f"clkout{n}_freq"] = freq
+                    config[f"clkout{n}_divide"] = d
+                    config[f"clkout{n}_phase"] = phase
+                    valid = True
+                    break
+
+            assert(valid), f"No valid parameters for frequency {freq}"
+
         return config
 
     def elaborate(self, platform):
         mmcm_fb = Signal()
 
-        obuf = Instance("BUFG",
-                i_I=self.clkout0,
-                o_O=self.clkout0_buf
-                )
+        self.config = self.make_config()
 
-        mmcme2 = Instance("MMCME2_ADV",
-                i_CLKFBIN=mmcm_fb,
-                i_CLKIN1=self.clkin,
-                i_RST=self.reset,
-                o_CLKFBOUT=mmcm_fb,
-                o_LOCKED=self.locked,
-                o_CLKOUT0=self.clkout0,
-                p_BANDWIDTH="OPTIMIZED",
-                p_CLKFBOUT_MULT_F=self.config["clkfbout_mult"],
-                p_CLKIN1_PERIOD=1e9/self.freq_in,
-                p_DIVCLK_DIVIDE=self.config["divclk_divide"],
-                p_REF_JITTER1=0.01,
-                p_CLKOUT0_DIVIDE_F=self.config["clkout0_divide"],
-                p_CLKOUT0_PHASE=self.config["clkout0_phase"]
-                )
+        params = {}
 
+        params.update(
+            i_CLKFBIN=mmcm_fb,
+            i_CLKIN1=self.clkin,
+            i_RST=self.reset,
+            o_CLKFBOUT=mmcm_fb,
+            o_LOCKED=self.locked,
+            p_BANDWIDTH="OPTIMIZED",
+            p_CLKFBOUT_MULT_F=self.config["clkfbout_mult"],
+            p_CLKIN1_PERIOD=1e9/self.freq_in,
+            p_DIVCLK_DIVIDE=self.config["divclk_divide"],
+            p_REF_JITTER1=0.01
+            )
 
-        m = Module()
-        m.submodules += [obuf, mmcme2]
-        return m
+        for n, (clk, freq, phase) in enumerate(self.outputs):
+            div = f"p_CLKOUT{n}_DIVIDE"
+            if n == 0:
+                div = div + "_F"
+
+            params[f"o_CLKOUT{n}"] = clk
+            params[div] = self.config[f"clkout{n}_divide"],
+            params[f"p_CLKOUT{n}_PHASE"] = self.config[f"clkout{n}_phase"]
+            #self.ports.append(clk)
+
+        print(f"ports = {self.ports}")
+
+        #self.m.submodules += Instance("MMCME2_ADV", **params)
+
+        return self.m
 
 if __name__ == '__main__':
     mmcme2 = MMCME2(12e6, 100e6)
+    mmcme2.create_clkout("clk192", 192e6, 0)
     main(mmcme2, ports=mmcme2.ports)
