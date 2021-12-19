cbd35606e451e5c5227a00264e78023b1a6ad45d
Bastian Löher <b.loeher@gsi.de>
2021-12-20 00:57:57 +0100

    tdc: Output values and rudimentary testbench.

--- tdc.py
+++ tdc.py
@@ -10,25 +10,61 @@ class Tdc(Elaboratable):
         self.clk_90 = Signal()
         self.input = Signal()
         self.time = Signal(32)
-        self.output = Signal(16)
-        self.w_en = Signal()
+        self.output = Signal(38)
+        self.rdy = Signal()
 
     def elaborate(self, platform):
 
         sample = Signal(4)
+        falling = Signal()
+        rising = Signal()
+        stable_on = Signal()
+        stable_off = Signal()
+        prev_last = Signal()
 
         os_in = OversamplingInput()
 
         m = Module()
+
+        m.domains += [
+                ClockDomain("fast")
+                ]
+
+        m.d.comb += [
+                ClockSignal("fast").eq(self.clk_0)
+                ]
 
         m.d.comb += [
                 os_in.clk_0.eq(self.clk_0),
                 os_in.clk_90.eq(self.clk_90),
                 os_in.input.eq(self.input),
                 sample.eq(os_in.data4),
+                stable_on.eq(os_in.data4 == 15),
+                stable_off.eq(os_in.data4 == 0),
+                falling.eq(((prev_last == 1) & (os_in.data4[0] == 0))
+                    | (os_in.data4[2:4] == C(1, 2))
+                    | (os_in.data4[1:3] == C(1, 2))
+                    | (os_in.data4[0:2] == C(1, 2))),
+                rising.eq(((prev_last == 0) & (os_in.data4[0] == 1))
+                    | (os_in.data4[2:4] == C(2, 2))
+                    | (os_in.data4[1:3] == C(2, 2))
+                    | (os_in.data4[0:2] == C(2, 2)))
                 ]
 
+        with m.If((falling == 1) | (rising == 1)):
+            # falling edge
+            m.d.fast += [
+                self.output.eq(Cat(Cat(Cat(sample, self.time), rising), falling))
+                ]
+        with m.Else():
+            # No change
+            m.d.fast += [
+                self.output.eq(C(0, unsigned(37)))
+                ]
 
+        m.d.fast += [
+                prev_last.eq(sample[3])
+                ]
 
         m.submodules += os_in
         return m
@@ -39,6 +75,8 @@ if __name__ == "__main__":
     clk_0 = Signal()
     clk_90 = Signal()
     i0 = Signal()
+    t = Signal(32)
+    out = Signal(38)
 
     m = Module()
     m.domains += ClockDomain("sync")
@@ -48,16 +86,51 @@ if __name__ == "__main__":
             dut.clk_0.eq(clk_0),
             dut.clk_90.eq(clk_90),
             dut.input.eq(i0),
-            fifo.w_data.eq(dut.output),
-            fifo.w_en.eq(dut.w_en),
+            dut.time.eq(t),
+            out.eq(dut.output)
             ]
 
     sim = Simulator(m)
 
+    stop = 50
+
+    def time():
+        for i in range(stop):
+            yield t.eq(t + 1)
+            yield
+
+    def clocks():
+        for i in range(stop):
+            yield clk_0.eq(~clk_0)
+            yield Delay(1/40e6)
+            yield clk_90.eq(~clk_90)
+            yield
+
     def proc():
         yield
 
+    def pulse(steps):
+        for i in range(steps):
+            yield i0.eq(1)
+            yield
+        yield i0.eq(0)
+        yield
+
+    def pause(steps):
+        for i in range(steps):
+            yield
+
+    def input():
+        for i in range(5):
+            yield from pulse(i)
+            yield from pause(5)
+
+
+
     sim.add_clock(1/20e6)
     sim.add_sync_process(proc)
+    sim.add_sync_process(time)
+    sim.add_sync_process(clocks)
+    sim.add_sync_process(input)
     with sim.write_vcd("tdc.vcd", "tdc.gtkw"):
         sim.run()
