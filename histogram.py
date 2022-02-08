4bfa243c67d35532f92de9d7b7aba44f594a6213
Bastian Löher <b.loeher@gsi.de>
2022-02-08 01:47:04 +0100

    Use Memory in histogram.

--- histogram.py
+++ histogram.py
@@ -16,27 +16,51 @@ class Histogram(Elaboratable):
         self.read = Signal()
 
         self.ports = (
-                self.index_r,
-                self.index_w,
-                self.data_r,
-                self.data_w,
-                self.increment,
-                self.write,
-                self.read
-                )
+            self.index_r,
+            self.index_w,
+            self.data_r,
+            self.data_w,
+            self.increment,
+            self.write,
+            self.read
+        )
 
     def elaborate(self, platform):
         m = Module()
 
-        # Writing
-        with m.If(self.increment == 1):
-            m.d.sync += self.mem[self.index_w].eq(self.mem[self.index_w] + 1)
-        with m.Elif(self.write == 1):
-            m.d.sync += self.mem[self.index_w].eq(self.data_w)
+        w_en = Signal()
+        w_data = Signal(self.bits)
+        r_addr = Signal(range(1, self.bins))
+        increment_delayed = Signal()
 
-        # Reading
-        with m.If(self.read == 1):
-            m.d.sync += self.data_r.eq(self.mem[self.index_r])
+        storage = Memory(width=self.bits, depth=self.bins)
+        w_port = m.submodules.w_port = storage.write_port()
+        r_port = m.submodules.r_port = storage.read_port()
+
+        m.d.comb += [
+            w_port.addr.eq(self.index_w),
+            w_port.data.eq(w_data),
+            w_port.en.eq(w_en),
+            r_port.addr.eq(r_addr),
+            self.data_r.eq(r_port.data),
+        ]
+
+        m.d.sync += increment_delayed.eq(self.increment)
+
+        m.d.comb += w_en.eq(self.write | increment_delayed)
+        m.d.comb += r_addr.eq(Mux(self.increment, self.index_w, self.index_r))
+        m.d.comb += w_data.eq(Mux(increment_delayed, r_port.data + 1, self.data_w))
+
+        # Old handling without memory
+        ## Writing
+        #with m.If(self.increment == 1):
+        #    m.d.sync += self.mem[self.index_w].eq(self.mem[self.index_w] + 1)
+        #with m.Elif(self.write == 1):
+        #    m.d.sync += self.mem[self.index_w].eq(self.data_w)
+
+        ## Reading
+        #with m.If(self.read == 1):
+        #    m.d.sync += self.data_r.eq(self.mem[self.index_r])
 
         return m
 
@@ -81,8 +105,13 @@ if __name__ == '__main__':
         yield from write(24, 4)
         yield from read(4)
 
-        print("dut.data_w = {}".format((yield dut.data_r)))
+        print("dut.data_r = {}".format((yield dut.data_r)))
         assert (yield dut.data_r) == 24, "Didn't get 24!"
+
+        yield from read(0)
+
+        print("dut.data_r = {}".format((yield dut.data_r)))
+        assert (yield dut.data_r) == 0, "Didn't get 0!"
 
         yield from increment(4)
         yield from read(4)
