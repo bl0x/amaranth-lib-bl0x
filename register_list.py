bd2dc621ba19d4b638c18edf5365d7e49f7260ef
Bastian Löher <b.loeher@gsi.de>
2022-11-24 00:38:49 +0100

    Add RegisterFile class.

--- register_list.py
+++ register_list.py
@@ -1,5 +1,35 @@
 import os
+import json
 from amaranth import Signal
+
+
+class RegisterFile():
+    def __init__(self, name):
+        self.name = name
+        self.tables = []
+
+    def add_table(self, table):
+        self.tables.append(table)
+
+    def to_json(self, filename):
+        registers = {}
+        for t in self.tables:
+            if t.shortname is not None:
+                name = t.shortname
+            else:
+                name = t.name
+            registers[name] = t.dict
+
+        with open(filename, "w") as f:
+            json.dump(registers, f)
+
+    def to_text(self, filename):
+        text = ""
+        for t in self.tables:
+            text += t.to_text()
+            text += "\n\n"
+        with open(filename, "w") as f:
+            f.write(text)
 
 
 class RegConf(dict):
@@ -14,8 +44,9 @@ class RegConf(dict):
 
 
 class RegisterTable():
-    def __init__(self, name):
+    def __init__(self, name, shortname=None):
         self.name = name
+        self.shortname = shortname
         self.dict = {}
 
     def add_list(self, name, offset, names):
@@ -33,28 +64,28 @@ class RegisterTable():
     def new_list_array(self, name):
         self.dict[name] = []
 
-
-def print_as_table(reg, name):
-    text = "Register list '{}'\n".format(name)
-    text += "----------------------------------------------\n"
-    text += "  addr bits    default    maximum name\n"
-    text += "----------------------------------------------\n"
-    for r1 in reg:
-        it = reg[r1]
-        if type(it) is list:
-            for i, item in enumerate(reg[r1]):
-                for r0 in reg[r1][i]:
-                    r = reg[r1][i][r0]
+    def to_text(self):
+        reg = self.dict
+        text = "Register table '{}'\n".format(self.name)
+        text += "----------------------------------------------\n"
+        text += "  addr bits    default    maximum name\n"
+        text += "----------------------------------------------\n"
+        for r1 in reg:
+            it = reg[r1]
+            if type(it) is list:
+                for i, item in enumerate(reg[r1]):
+                    for r0 in reg[r1][i]:
+                        r = reg[r1][i][r0]
+                        maximum = (1 << r.bits)
+                        text += "0x{:04x}: {:3d} {:10d} {:10d} {}\n".format(
+                                r.addr, r.bits, r.reset, maximum, r.name)
+            else:
+                for r0 in reg[r1]:
+                    r = reg[r1][r0]
                     maximum = (1 << r.bits)
                     text += "0x{:04x}: {:3d} {:10d} {:10d} {}\n".format(
                             r.addr, r.bits, r.reset, maximum, r.name)
-        else:
-            for r0 in reg[r1]:
-                r = reg[r1][r0]
-                maximum = (1 << r.bits)
-                text += "0x{:04x}: {:3d} {:10d} {:10d} {}\n".format(
-                        r.addr, r.bits, r.reset, maximum, r.name)
-    return text
+        return text
 
 
 def register_list(listname, offset, names):
