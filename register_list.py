58193023f61cb0e99ec7710abe91ca98b7c1d860
Bastian Löher <b.loeher@gsi.de>
2022-11-24 00:17:14 +0100

    Add RegisterTable class.

--- register_list.py
+++ register_list.py
@@ -3,13 +3,35 @@ from amaranth import Signal
 
 
 class RegConf(dict):
-    def __init__(self, addr, bits, signame, reset=0):
+    def __init__(self, addr, bits, signame, reset=0, description=None):
         self.addr = addr
         self.reset = reset
         self.bits = bits
         self.name = signame
         self.signal = Signal(bits, name=signame, reset=self.reset)
-        dict.__init__(self, addr=addr, reset=reset, bits=bits, name=signame)
+        dict.__init__(self, addr=addr, reset=reset, bits=bits, name=signame,
+                      description=description)
+
+
+class RegisterTable():
+    def __init__(self, name):
+        self.name = name
+        self.dict = {}
+
+    def add_list(self, name, offset, names):
+        if name in self.dict:
+            if type(self.dict[name]) is not list:
+                print(("{} already exists in register table {} and it "
+                       "is not a list, but a {}.").format(
+                           name, self.name, type(self.dict[name])))
+                os.abort()
+            self.dict[name].append(register_list(
+                name+str(len(self.dict[name])), offset, names))
+        else:
+            self.dict[name] = register_list(name, offset, names)
+
+    def new_list_array(self, name):
+        self.dict[name] = []
 
 
 def print_as_table(reg, name):
