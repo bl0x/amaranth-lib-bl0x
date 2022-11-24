0b32075824575522e4664d278aae987eb24a212a
Bastian Löher <b.loeher@gsi.de>
2022-11-24 01:37:53 +0100

    Add description handling.

--- register_list.py
+++ register_list.py
@@ -1,5 +1,6 @@
 import os
 import json
+import textwrap
 from amaranth import Signal
 
 
@@ -48,8 +49,9 @@ class RegisterTable():
         self.name = name
         self.shortname = shortname
         self.dict = {}
+        self.description = {}
 
-    def add_list(self, name, offset, names):
+    def add_list(self, name, offset, description=None, names=None):
         if name in self.dict:
             if type(self.dict[name]) is not list:
                 print(("{} already exists in register table {} and it "
@@ -58,33 +60,42 @@ class RegisterTable():
                 os.abort()
             self.dict[name].append(register_list(
                 name+str(len(self.dict[name])), offset, names))
+            if description is not None:
+                print("Description not used.")
+                print("Add description to new_list_array call.")
         else:
             self.dict[name] = register_list(name, offset, names)
+            self.description[name] = description
 
-    def new_list_array(self, name):
+    def new_list_array(self, name, description=None):
         self.dict[name] = []
+        self.description[name] = description
+
+    def reg_to_text(self, r):
+        maximum = (1 << r.bits)
+        return "0x{:04x}: {:3d} {:10d} {:10d} {}\n".format(
+                r.addr, r.bits, r.reset, maximum, r.name)
 
     def to_text(self):
         reg = self.dict
+        desc = self.description
         text = "Register table '{}'\n".format(self.name)
-        text += "----------------------------------------------\n"
+        text += "----------------------------------------------------------\n"
         text += "  addr bits    default    maximum name\n"
-        text += "----------------------------------------------\n"
+        text += "----------------------------------------------------------\n"
         for r1 in reg:
             it = reg[r1]
+            text += "\n"
+            if desc[r1] is not None:
+                wrapped = textwrap.indent(textwrap.fill(desc[r1]), "  #### ")
+                text += "{}\n\n".format(wrapped)
             if type(it) is list:
                 for i, item in enumerate(reg[r1]):
                     for r0 in reg[r1][i]:
-                        r = reg[r1][i][r0]
-                        maximum = (1 << r.bits)
-                        text += "0x{:04x}: {:3d} {:10d} {:10d} {}\n".format(
-                                r.addr, r.bits, r.reset, maximum, r.name)
+                        text += self.reg_to_text(reg[r1][i][r0])
             else:
                 for r0 in reg[r1]:
-                    r = reg[r1][r0]
-                    maximum = (1 << r.bits)
-                    text += "0x{:04x}: {:3d} {:10d} {:10d} {}\n".format(
-                            r.addr, r.bits, r.reset, maximum, r.name)
+                    text += self.reg_to_text(reg[r1][r0])
         return text
 
 
@@ -94,11 +105,15 @@ def register_list(listname, offset, names):
         if len(conf) == 2:
             name, bits = conf
             reset = 0
+            description = None
         elif len(conf) == 3:
             name, bits, reset = conf
+            description = None
+        elif len(conf) == 4:
+            name, bits, reset, description = conf
         else:
-            print("ERROR: register item must be a 2 or 3 tuple.")
+            print("ERROR: register item must be a 2, 3 or 4 tuple.")
             os.abort()
         signame = listname + "_" + name
-        regs[name] = RegConf(offset + i, bits, signame, reset)
+        regs[name] = RegConf(offset + i, bits, signame, reset, description)
     return regs
