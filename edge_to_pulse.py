9cd0f83348fe85d3fbf5da3c7c19d3802d33bd1a
Bastian Loeher <bastian.loeher@smithsdetection.com>
2024-03-16 21:32:39 +0100

    Update EdgeToPulse to current Amaranth language.

--- edge_to_pulse.py
+++ edge_to_pulse.py
@@ -1,20 +1,19 @@
 from amaranth import *
+from amaranth.lib import wiring
+from amaranth.lib.wiring import In, Out
 from amaranth.sim import *
 
 from edge_detect import EdgeDetector
 
-class EdgeToPulse(Elaboratable):
+class EdgeToPulse(wiring.Component):
+
+    o: Out(1)
+    i: In(1)
+
     def __init__(self, bits=16):
         self.width = Signal(bits)
         self.bits = bits
-
-        self.input = Signal()
-        self.output = Signal()
-
-        self.ports = [
-            self.input,
-            self.output
-        ]
+        super().__init__()
 
     def elaborate(self, platform):
         ed = EdgeDetector()
@@ -22,12 +21,12 @@ class EdgeToPulse(Elaboratable):
 
         m = Module()
 
-        m.d.comb += ed.input.eq(self.input)
+        m.d.comb += ed.input.eq(self.i)
 
         with m.If(ed.rose == 1):
             m.d.sync += counter.eq(self.width - 1)
 
-        m.d.comb += self.output.eq((ed.rose == 1) | (counter > 0))
+        m.d.comb += self.o.eq((ed.rose == 1) | (counter > 0))
 
         with m.If(counter > 0):
             m.d.sync += counter.eq(counter - 1),
@@ -38,33 +37,32 @@ class EdgeToPulse(Elaboratable):
 
 
 if __name__=="__main__":
-    dut = EdgeToPulse(width = 2)
-    sim = Simulator(dut)
+    dut = EdgeToPulse(bits=2)
 
     def strobe():
-        yield dut.input.eq(1)
+        yield dut.i.eq(1)
         yield
-        yield dut.input.eq(0)
+        yield dut.i.eq(0)
         yield
 
     def proc():
-        assert((yield dut.output) == 0)
+        assert((yield dut.o) == 0)
         yield
-        yield dut.input.eq(1)
-        yield
-        yield
-        assert((yield dut.output) == 1)
-        yield dut.input.eq(0)
+        yield dut.i.eq(1)
         yield
         yield
-        assert((yield dut.output) == 0)
+        assert((yield dut.o) == 1)
+        yield dut.i.eq(0)
+        yield
+        yield
+        assert((yield dut.o) == 0)
 
         for i in range(10 * 2):
             yield from strobe()
             yield
 
+    sim = Simulator(dut)
     sim.add_clock(1/12e6)
     sim.add_sync_process(proc)
-    with sim.write_vcd('edge_to_pulse.vcd', 'edge_to_pulse_orig.gtkw',
-            traces=dut.ports):
+    with sim.write_vcd('edge_to_pulse.vcd', 'edge_to_pulse_orig.gtkw'):
         sim.run()
